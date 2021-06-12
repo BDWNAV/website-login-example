2d392882f377bedf34325abeb655c071629ba69e
BDWNAV <rs736611@gmail.com>
2021-06-12 17:37:09 +1000

    Update createaccountRoute.js

--- src/routes/createaccountRoute.js
+++ src/routes/createaccountRoute.js
@@ -9,9 +9,20 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
     let userData = await userSchema.findOne({
         username: req.body.username,
-
+        email: req.body.email
     });
-    res.redirect(`/user/${newUser._id}`);
+
+    if(userData) {
+        res.send("There is already user data for you");
+    } else {
+        const newUser = new userSchema({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+        newUser.save();
+        res.redirect(`/user/${newUser._id}`);
+    }
 });
 
 module.exports = router;
